dd046cd90fee72a0ad0bc13af1e40fe77f00f8df
Kieran Winfield <K.Winfield@ucas.ac.uk>
2020-01-06 14:13:34 +0000

    Got sass working on the webpage

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 'use strict';
 // JavaScript containing shared constants, for use in all pages.
 
-var backendIp = 'backend IP goes here';
-var token = 'your token goes here';
+var backendIp = '34.248.103.39';
+var token = '5e7f6416-786a-430a-83fc-90acd7f5b3b2';
 
 function buildUrl(path) {
     return 'http://' + backendIp + '/images' + path + '?token=' + token;
